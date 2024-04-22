ef6fe41b4d313bf91048c69ae77e97cb2e0ccf31
unknown <kasalawotkiran@gmail.com>
2024-04-22 07:36:15 +0545

    new

---
+++ InputOutput.java
@@ -0,0 +1,13 @@
+public class InputOutput {
+    public static void main(String[] args) {
+       /* System.out.println(null);
+        'sout'
+       */ 
+      System.out.println("This line breaks");
+      /*System.out.print */
+      System.out.print("This will not break");
+      System.out.print("This will continue\n");
+
+    }
+    
+}
